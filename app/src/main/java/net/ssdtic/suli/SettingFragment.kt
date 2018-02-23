b8dd255b0ccd135c9e65c69e143dba2209cf0f77
ssd <ssd_chan@yahoo.co.jp>
2018-02-24 04:43:17 +0900

    create setting fragment

--- app/src/main/java/net/ssdtic/suli/SettingFragment.kt
+++ app/src/main/java/net/ssdtic/suli/SettingFragment.kt
@@ -1,20 +1,43 @@
 package net.ssdtic.suli
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceManager
+import android.content.SharedPreferences
 
 
-class SettingFragment : Fragment() {
+class SettingFragment : PreferenceFragmentCompat() {
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
+    companion object {
+        const val PREF_KEY_AUTO_SYNC = "pref_key_auto_sync"
+        const val PREF_KEY_ADS_DISPLAY = "pref_key_ads_display"
+    }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.fragment_setting, null)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        // Load the preferences from an XML resource
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        // Get the preference data
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(activity)
+        // true is initialize value then not got value
+        println("PREF_KEY_AUTO_SYNC : " + sharedPref.getBoolean(SettingFragment.PREF_KEY_AUTO_SYNC, true))
+        println("PREF_KEY_ADS_DISPLAY : " + sharedPref.getBoolean(SettingFragment.PREF_KEY_ADS_DISPLAY, true))
+    }
+
+    private fun myChangeListener(key: String) {
+        println("change preference : " + key)
+    }
+
+    private val listener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key -> myChangeListener(key) }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener)
     }
 
 }
