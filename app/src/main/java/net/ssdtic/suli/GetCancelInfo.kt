a7945e527488fcceaffc5e5567932b7126ee05be
ssd <ssd_chan@yahoo.co.jp>
2018-03-27 22:21:33 +0900

    create get cancelinfo function

--- app/src/main/java/net/ssdtic/suli/GetCancelInfo.kt
+++ app/src/main/java/net/ssdtic/suli/GetCancelInfo.kt
@@ -1,49 +1,124 @@
 package net.ssdtic.suli
 
-import android.util.Log
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.result.Result
+import io.realm.Realm
+import org.jsoup.Jsoup
+
 
 class GetCancelInfo {
 
     companion object {
 
-        private val cancelInfoUrl = MyApplication.getContext()?.applicationContext?.getString(R.string.address_cancelInfo)
+        private val cancelInfoUrl = MyApplication.getContext()?.getString(R.string.address_cancelInfo) ?: ""
+
+        private var opt :Request? = null
 
         fun start(completeHandler: () -> Unit, errorHandler: (String) -> Unit) {
 
-            Log.d("Info", "GetCancelInfo : start task $cancelInfoUrl")
+            println("GetCancelInfo : start task")
 
-            getData(1, emptyArray(), completeHandler, errorHandler)
+            getData(1, emptyArray(), { writeData ->
+
+                //Realmに接続
+                val realm = Realm.getDefaultInstance()
+                //CancelInfoのすべてのオブジェクトを取得
+                val cancelInfo = realm.where(CancelInfo::class.java).findAll()
+
+                realm.executeTransaction {
+                    //取得したすべてのオブジェクトを削除
+                    cancelInfo.deleteAllFromRealm()
+                    println("GetCancelInfo : all cancelInfo data delete")
+
+                    for ((i,data) in writeData.withIndex()) {
+                        //マルチスレッドで処理したのでIDが競合するのを防ぐためにここで設定する
+                        data.id = i + 1
+                        //データを書き込む
+                        realm.copyToRealm(data)
+                    }
+                    println("GetCancelInfo : all data is written")
+                }
+
+                println("GetCancelInfo : all task complete")
+                completeHandler()
+            }, { message ->
+                print("GetCancelInfo : failed task")
+                errorHandler(message)
+            })
         }
 
-        fun cancel() {
-            Log.d("Info", "GetCancelInfo : cancel task")
-        }
-
-        private fun getData(page: Int, data: Array<CancelInfo>, completeHandler: () -> Unit, errorHandler: (String) -> Unit) {
+        private fun getData(page: Int, data: Array<CancelInfo>, completeHandler: (Array<CancelInfo>) -> Unit, errorHandler: (String) -> Unit) {
 
             var writeData : Array<CancelInfo> = data
 
-            Log.d("Info", "GetCancelInfo : No.$page data init")
+            try {
+                println("GetCancelInfo : No.$page data init")
 
-            Fuel.get(cancelInfoUrl!!, listOf("abspage" to page)).response { request, response, result ->
-                when (result) {
-                    is Result.Success -> {
-                        // レスポンスボディを表示
-                        println("非同期処理の結果：" + String(response.data))
-                    }
-                    is Result.Failure -> {
-                        println("通信に失敗しました。")
+                opt = Fuel.get(cancelInfoUrl, listOf("abspage" to page)).response { request, response, result ->
+
+                    when (result) {
+
+                        is Result.Failure -> {
+                            val err = result.error
+                            println("GetCancelInfo : No.$page data failed ${err.localizedMessage}")
+                            errorHandler(err.localizedMessage)
+                        }
+
+                        is Result.Success -> {
+
+                            Jsoup.parse(response.toString()).select(".table_data tr").let { doc ->
+
+                                if (doc.size >= 2) {
+                                    for (i in 1 until doc.size) {
+                                        val tdNodes = doc[i].select("td")
+
+                                        // 書き込むデータを作成
+                                        val data = CancelInfo()
+
+                                        data.date = tdNodes[0].text()
+                                        data.classification = tdNodes[1].text()
+                                        data.time = tdNodes[2].text()
+                                        data.department = tdNodes[3].text()
+                                        data.classname = tdNodes[4].text()
+                                        data.person = tdNodes[5].text()
+                                        data.place = tdNodes[6].text()
+                                        data.note = tdNodes[7].text()
+
+                                        writeData.plus(data)
+                                    }
+
+                                }
+
+                                println("GetCancelInfo : No.$page data complete")
+                            }
+
+                            Jsoup.parse(response.toString()).select(".prevnextpage")?.let { doc ->
+
+                                if (doc.size < (if (page == 1) 1 else 2)) {
+                                    //これ以上読み込むページがない(処理が完了)
+                                    completeHandler(writeData)
+                                }
+                                else {
+                                    //次のページを読み込む
+                                    getData(page + 1, writeData, completeHandler, errorHandler)
+                                }
+                            } ?: run {
+                                errorHandler("GetCancelInfo: can't get next page flag")
+                            }
+                        }
+
                     }
                 }
+
+            } catch (error: Exception) {
+                println("got an error creating the request: $error")
+                errorHandler(error.localizedMessage)
             }
+        }
 
-            completeHandler()
-            Log.d("Info", "run error handler")
-            errorHandler("test")
-
+        fun cancel() {
+            opt?.cancel()
         }
     }
 }
